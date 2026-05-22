e834621c0f4f62e453fe76ee28f8606b75813c39
JoyceHanan <joycehananmarri@gmail.com>
2026-05-22 15:33:21 +0530

    Update multer configuration to support additional file types and increase file size limit

--- backend/config/multer.js
+++ backend/config/multer.js
@@ -1,20 +1,26 @@
 import multer from "multer";
-
 export const upload = multer({
-  //store in RAM
-  storage: multer.memoryStorage(),
-  //to avoid RAM overflow
-  limits: {
-    fileSize: 2 * 1024 * 1024, // 2MB
-  },
-  //for security validation
-  fileFilter: (req, file, cb) => {
-    if (file.mimetype === "image/jpeg" || file.mimetype === "image/png") {
-      cb(null, true);
-    } else {
-      const err = new Error("Only JPG and PNG allowed");
-      err.status = 400;
-      cb(err, false);
+    // Store in RAM
+    storage: multer.memoryStorage(),
+    // Avoid RAM overflow
+    limits:{fileSize:10*1024*1024},
+    // File validation
+    fileFilter:(req,file,cb)=>{
+        const allowedTypes=[
+            "image/jpeg",
+            "image/png",
+            "application/pdf",
+            "application/vnd.ms-powerpoint",
+            "application/vnd.openxmlformats-officedocument.presentationml.presentation"
+        ];
+        if(allowedTypes.includes(file.mimetype)
+        ){
+            cb(null,true);
+        }
+        else{
+            const err=new Error("Only JPG, PNG, PDF, PPT, PPTX allowed");
+            err.status=400;
+            cb(err,false);
+        }
     }
-  },
 });
